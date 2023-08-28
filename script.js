0aec79dc073b13893d48770ebcb8cdebd2ef6ffc
zmthall <74847979+zmthall@users.noreply.github.com>
2023-08-28 14:46:30 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,20 +2,54 @@ const stickyItem = document.querySelector("[data-sticky]");
 const buttons = document.querySelectorAll("[data-button]");
 var topScreen = false;
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        if(button.getAttribute("data-button") === "languages") {
-            button.classList.toggle("active");
+// listening for clicks within the entire window to close containers that open from using .active class
+window.addEventListener("click", (e) => {
+    const languageDropdown = document.querySelector(".languages-dropdown-container");
+    const mobileLanguage = document.querySelector(".mobile-languages-container"); 
+    const mobileNav = document.querySelector(".mobile-content-nav-container");
+    buttons.forEach(button => {
+        if(button != e.target && button.classList.contains("active")) {
+            if(languageDropdown.contains(e.target) 
+                && button.getAttribute("data-button") === "languages") {
+                return;
+            } else if(mobileNav.contains(e.target) 
+                && button.getAttribute("data-button") === "hamburger-nav") {
+                return;
+            } else if(mobileLanguage.contains(e.target)
+                && button.getAttribute("data-button") === "mobile-languages") {
+                return;
+            }
+            else {
+                button.classList.toggle("active");
+            }
         }
     })
 })
 
+// listening for clicks within any button to add the .active class to the button
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        button.classList.toggle("active");
+    })
+})
+
+// listening when the window is being resized to reset .active class when the screen shrinks to a smaller screen
+window.addEventListener("resize", () => {
+    let temp = document.querySelectorAll(".active");
+    let width = screen.width;
+    if(width <= 960) {
+        temp.forEach(item => {
+            item.classList.remove("active");
+        })
+    }
+})
+
+// Intersection observer for when the booking information container intersects with the header
 const topOptions = {
     root: null,
     threshold: 1,
     rootMargin: "-84px 0px 0px 0px"
 };
-
 const topObserver = new IntersectionObserver(entries => {
     const temp = document.querySelector(".languages-dropdown-container");
     entries.forEach(entry => {
@@ -38,6 +72,7 @@ const topObserver = new IntersectionObserver(entries => {
     })
 }, topOptions)
 
+// Intersection observer for when the booking information container gets close to the header language dropdown nav element
 const options = {
     root: null,
     threshold: 1,
